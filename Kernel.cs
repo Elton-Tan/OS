d5278ea0711cb5f9c4cd57e50fa5043acc2a0324
Elton <83337647+Elton-Tan@users.noreply.github.com>
2024-01-28 11:00:44 +0800

    Add files via upload

---
+++ Kernel.cs
@@ -0,0 +1,152 @@
+﻿using Cosmos.Core;
+using Cosmos.System.Graphics;
+using System;
+using System.Threading;
+using Display = TangerineOS.GUI.GUI;
+using Sys = Cosmos.System;
+
+namespace TangerineOS
+{
+    public class Kernel : Sys.Kernel
+    {
+        public static readonly string OSVERSION = "TangerineOS Version Alpha 0.5.1";
+        public static readonly string OSNAME = "TangerineOS";
+        public static string PCNAME = "pc";
+        public static readonly string MAINDISK = "0:\\";
+        public static readonly string SYSTEMPATH = MAINDISK + "TangerineOS\\";
+        public static readonly string USERSPATH = SYSTEMPATH + "Users\\";
+
+        public static readonly string PROGRAMSPATH = SYSTEMPATH + "Programs\\";
+        public static readonly string PROGRAMSDATAPATH = SYSTEMPATH + "Config\\";
+        public static readonly string SYSTEMCONFIG = PROGRAMSDATAPATH + "systemConfig.cfg";
+
+        public static readonly string CURRENTUSER = USERSPATH + "Admin\\";
+        public static readonly string USERPROGRAMSPATH = CURRENTUSER + "Programs\\";
+        public static readonly string USERPROGRAMSDATAPATH = CURRENTUSER + "Config\\";
+        public static readonly string USERCONFIG = USERPROGRAMSDATAPATH + "userConfig.cfg";
+
+        public static bool GUIenabled = false;
+        public static bool useDisks = true;
+        public static bool useNetwork = false;
+        public static bool safeMode = false;
+
+        //BeforeRun() is the display before the OS start.
+        protected override void BeforeRun()
+        {
+            try
+            {
+                Console.Clear(); //clear console of all data
+                //Console.CursorVisible = false;
+                Console.CursorLeft = 35;
+                Console.CursorTop = 11;
+                Console.ForegroundColor = ConsoleColor.Yellow; // set console to green
+                Console.WriteLine("TangerineOS"); //Our OS name
+                Console.CursorLeft = 0;
+                Console.CursorTop = Console.WindowHeight - 1;
+                TextMode.Run();
+            }
+            catch (Exception e)
+            {
+                SystemCrash(e.Message);
+            }
+
+        }
+        protected override void Run()
+        {
+            try
+            {
+                if (GUIenabled) { Display.Refresh(); } //because we cleared the display before, we will refresh it
+                else { TextMode.ConsoleMode(); }
+            }
+            //if the system encounter error when starting
+            catch (Exception e) 
+            {
+                try
+                {
+                    if (GUIenabled)
+                    { Display.Refresh(); Msg.Main("Error", "System " + e, GUI.Icons.error); } //Display error
+                    else { Console.WriteLine("System " + e, GUI.Icons.error); } //Show error in console
+                }
+                //if cant even load the GUI
+                catch (Exception ex)
+                {
+                    SystemCrash(ex.ToString()); //then we will crash the system and catch the error
+                }
+            }
+        }
+
+        //happens when system crash
+        public static void SystemCrash(string e)
+        {
+            Display.canvas?.Disable();
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.CursorTop = Console.WindowHeight / 3;
+            Console.CursorLeft = (Console.WindowWidth - OSVERSION.Length - 4) / 2;
+            Console.Write("  " + OSVERSION + "  ");
+            Thread.Sleep(500);
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.CursorLeft = (Console.WindowWidth - ("Fatal System " + e).Length) / 2;
+            Console.Write("Fatal System " + e);
+            Thread.Sleep(500);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.CursorLeft = (Console.WindowWidth - "Press any key to restart computer ".Length) / 2;
+            Console.Write("Press any key to restart computer ");
+            Console.ReadKey(true); //awat until a kew is pressed and read
+            Sys.Power.Reboot();//then reboot the system
+            ACPI.Reboot();
+        }
+
+        //Shutting down / Restarting the OS
+        public static void Shutdown(bool restart = false, bool force = false)
+        {
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.CursorVisible = false;
+            Console.Clear();
+            Console.SetCursorPosition(18, Console.WindowHeight / 2 - 1);
+            if (force)
+            {
+                if (restart) { Sys.Power.Reboot(); }  //if user select restart, OS reboots
+                else { Sys.Power.Shutdown(); } //if not it will just shutdown for good
+            }
+            else
+            {
+                if (GUIenabled)
+                {
+                    Display.ShutdownGUI(restart);//we shutdown the GUI
+                }
+                Console.Beep(700, 100);
+                Console.Beep(600, 100);
+                Console.Beep(500, 100);
+                if (restart)
+                {
+                    Console.Beep(400, 300);
+                    if (GUIenabled) { Display.canvas.Disable(); }
+                    Thread.Sleep(100);
+                    Sys.Power.Reboot();
+                }
+                else
+                {
+                    Console.Beep(400, 100);
+                    Console.Beep(300, 200);
+                    if (GUIenabled) { Display.canvas.Disable(); }
+                    Thread.Sleep(100);
+                    Sys.Power.Shutdown();
+                }
+            }
+            Console.Write("It is now safe to exit the virtual machine.");
+            while (true)
+            {
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
---
+++ Shell.cs
@@ -0,0 +1,195 @@
+﻿using Cosmos.Core;
+using Cosmos.Core.Memory;
+using Cosmos.HAL;
+using Sys = Cosmos.System;
+using Cosmos.System.Graphics;
+using Cosmos.System.Graphics.Fonts;
+using Microsoft.VisualBasic;
+using TangerineOS.Commands;
+using System;
+using System.Drawing;
+using System.Threading;
+using Display = TangerineOS.GUI.GUI;
+using System.Collections.Generic;
+
+namespace TangerineOS
+{
+    public class TextMode
+    {
+        private static bool useGUI = true;
+        private static string err = "OK";
+
+        private static string input;
+        private static CommandShell shell;
+        private static List<string> history = new List<string>();
+        public static void Run()
+        {
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.CursorTop = Console.WindowHeight - 1;
+            Console.Write("Hold Shift for advanced options");
+            //Console.CursorVisible = true;
+            Console.Beep(300, 200);
+            Thread.Sleep(50);
+            Console.Beep(400, 100);
+            Thread.Sleep(20);
+            Console.Beep(500, 100);
+            Thread.Sleep(20);
+            Console.Beep(600, 100);
+            Thread.Sleep(20);
+            Console.Beep(700, 100);
+
+            Console.CursorLeft = 0;
+            Console.Write("                               ");
+            Console.CursorLeft = 0;
+            Console.ResetColor();
+            //Console.CursorVisible = false;
+
+            if (Sys.KeyboardManager.ShiftPressed) { Console.Beep(); BootMenu(); }
+
+            CommandManager.Register();
+
+            if (Kernel.safeMode) { Sysinfo.CPUname = "Unknown"; }
+            else
+            {
+                Sysinfo.CPUname = CPU.GetCPUBrandString();
+                if (Kernel.useDisks) { FileManager.Start(); }
+            }
+            
+            if (useGUI)
+            {
+                err = Display.Init();
+                if (err != "OK") { useGUI = false; BootMenu(); }
+            }
+        }
+        public static void BootMenu()
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.CursorLeft = (Console.WindowWidth - Kernel.OSVERSION.Length + 4) / 2;
+                Console.WriteLine("  " + Kernel.OSVERSION + "  ");
+                Console.ResetColor();
+                if (err != "OK") { Console.WriteLine("Failed to initialize GUI\n" + err); }
+
+                Console.Write("\nESC - Shutdown System\nEnter - Start System Normally\nQ - GUI Safe Mode\nC - Console (Text) Mode\n\nSelect option: ");
+                try
+                {
+                    ConsoleKeyInfo cki = Console.ReadKey();
+                    switch (cki.Key)
+                    {
+                        case ConsoleKey.Escape:
+                            Kernel.Shutdown();
+                            while (true) ;
+                        case ConsoleKey.Q:
+                            Kernel.safeMode = true;
+                            Kernel.useDisks = false;
+                            useGUI = true;
+                            return;
+                        case ConsoleKey.Enter:
+                            useGUI = true;
+                            return;
+                        case ConsoleKey.C:
+                            useGUI = false;
+                            return;
+                    }
+                }
+                catch (Exception e)
+                {
+                    err = Convert.ToString(e);
+                }
+            }
+        }
+        public static void ConsoleMode()
+        {
+            if(shell == null)
+            {
+                shell = new CommandShell { crashClient = ExecuteError, update = Result };
+                Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Clear();
+                Console.ResetColor();
+                //Console.CursorVisible = true;
+                Console.WriteLine(Kernel.OSVERSION + "\n");
+            }
+            //yPos = Console.CursorTop + 1;
+            shell.Execute(Input());
+            Heap.Collect();
+        }
+        private static void ExecuteError()
+        {
+            throw new Exception("Manual crash");
+        }
+        private static void Result()
+        {
+            Console.WriteLine(shell.print);
+        }
+        private static string Input()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("\n" + shell.prompt + " ");
+            input = "";
+            int position = 0;
+            while (true)
+            {
+                ConsoleKeyInfo name = Console.ReadKey();
+                switch (name.Key)
+                {
+                    case ConsoleKey.Delete:
+                        if (Sys.KeyboardManager.ControlPressed && Sys.KeyboardManager.AltPressed) { Cosmos.System.Power.Reboot(); }
+                        break;
+                    case ConsoleKey.Escape:
+                        Console.ResetColor();
+                        Console.WriteLine();
+                        return null;
+                    case ConsoleKey.Enter:
+                        Console.ResetColor();
+                        Console.WriteLine();
+                        history.Add(input);
+                        return input;
+                    case ConsoleKey.UpArrow:
+                        if (history.Count - position > 0)
+                        {
+                            position++;
+                            Console.CursorLeft = 0;
+                            Console.Write(new string(' ', Console.WindowWidth - 1));
+                            Console.CursorTop--;
+                            Console.Write("\n" + shell.prompt + " ");
+                            input = history[history.Count - position];
+                            Console.Write(input);
+                        }
+                        break;
+                    case ConsoleKey.F1:
+                        Console.ResetColor();
+                        Console.WriteLine();
+                        return "help";
+                    case ConsoleKey.DownArrow:
+                        if (position > 1)
+                        {
+                            position--;
+                            Console.CursorLeft = 0;
+                            Console.Write(new string(' ', Console.WindowWidth - 1));
+                            Console.CursorTop--;
+                            Console.Write("\n" + shell.prompt);
+                            input = history[history.Count - position];
+                            Console.Write(input);
+                        }
+                        break;
+                    case ConsoleKey.Backspace:
+                        if (Console.CursorLeft > shell.prompt.Length + 1)
+                        {
+                            Console.CursorLeft--;
+                            Console.Write(" ");
+                            input = input.Remove(input.Length - 1);
+                            Console.CursorLeft--;
+                        }
+                        break;
+                    default:
+                        input += name.KeyChar;
+                        break;
+                }
+            }
+        }
+    }
+}
